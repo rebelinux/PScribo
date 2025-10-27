58c1dcda0f43d4e630a5abc4ea5a321ea791b34c
Jonathan Colon <rebelinux@gmail.com>
2025-10-27 14:24:24 -0400

    Fix formatting and add skip conditions for image type tests in ConvertTo-Image

--- Tests/Unit/ConvertTo-Image.Tests.ps1
+++ Tests/Unit/ConvertTo-Image.Tests.ps1
@@ -1,5 +1,5 @@
 ﻿$here = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
-$testRoot  = Split-Path -Path $here -Parent
+$testRoot = Split-Path -Path $here -Parent
 $moduleRoot = Split-Path -Path $testRoot -Parent
 Import-Module -Name "$moduleRoot\PScribo.psm1" -Force
 
@@ -9,7 +9,7 @@ InModuleScope -ModuleName 'PScribo' -ScriptBlock {
 
     Describe -Name 'ConvertTo-Image' -Fixture {
 
-        It "returns 'System.Drawing.Image' type" {
+        It "returns 'System.Drawing.Image' type" -Skip:$($PSVersionTable.Platform -ne 'Unix') {
             $testPath = Join-Path -Path $testRoot -ChildPath 'TestImage.jpg'
             $testUri = Resolve-ImageUri -Path $testPath
             $testBytes = Get-UriBytes -Uri $testUri
@@ -17,6 +17,15 @@ InModuleScope -ModuleName 'PScribo' -ScriptBlock {
             $result = ConvertTo-Image -Bytes $testBytes
 
             $result -is [System.Drawing.Image] | Should Be $true
+        }
+        It "returns 'SixLabors.ImageSharp.Image' type" -Skip:$($PSVersionTable.Platform -eq 'Unix') {
+            $testPath = Join-Path -Path $testRoot -ChildPath 'TestImage.jpg'
+            $testUri = Resolve-ImageUri -Path $testPath
+            $testBytes = Get-UriBytes -Uri $testUri
+
+            $result = ConvertTo-Image -Bytes $testBytes
+
+            $result -is [SixLabors.ImageSharp.Image] | Should Be $true
         }
 
     }
